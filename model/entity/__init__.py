9bd9bfe9c792ff42fff49a793f632536312c668e
MEssbah.A <messbah.a@gmail.com>
2023-12-04 11:30:42 +0330

    flask-sqlalchemy tested

--- model/entity/__init__.py
+++ model/entity/__init__.py
@@ -2,4 +2,5 @@ from model.entity.base import Base
 from model.entity.user import User
 from model.entity.comment import Comment
 from model.entity.post import Post
+from model.entity.like import Like
 
